02d97916fd2f0470b6c5a9121994f6561e0f64d9
Konstantin <pikula.konstantin@yandex.ru>
2023-07-06 09:10:17 +0300

    fix <Link/>

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,58 @@
-import Loadable from 'react-loadable';
-import React, { Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import Loadable from "react-loadable";
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
+import Home from "./pages/Home";
 
-import './scss/app.scss';
-import MainLayout from './layouts/MainLayout';
+import "./scss/app.scss";
+import MainLayout from "./layouts/MainLayout";
 
 const Cart = Loadable({
-  loader: () => import(/* webpackChunkName: "Cart" */ './pages/Cart'),
-  loading: () => <div>Идёт загрузка корзины...</div>,
+    loader: () => import(/* webpackChunkName: "Cart" */ "./pages/Cart"),
+    loading: () => <div>Идёт загрузка корзины...</div>,
 });
 
-const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'));
-const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
+const FullPizza = React.lazy(
+    () => import(/* webpackChunkName: "FullPizza" */ "./pages/FullPizza")
+);
+const NotFound = React.lazy(
+    () => import(/* webpackChunkName: "NotFound" */ "./pages/NotFound")
+);
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route path="" element={<Home />} />
-        <Route
-          path="cart"
-          element={
-            <Suspense fallback={<div>Идёт загрузка корзины...</div>}>
-              <Cart />
-            </Suspense>
-          }
-        />
-        <Route
-          path="pizza/:id"
-          element={
-            <Suspense fallback={<div>Идёт загрузка...</div>}>
-              <FullPizza />
-            </Suspense>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <Suspense fallback={<div>Идёт загрузка...</div>}>
-              <NotFound />
-            </Suspense>
-          }
-        />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/pizza-delivery" element={<MainLayout />}>
+                <Route path="" element={<Home />} />
+                <Route
+                    path="/pizza-delivery/cart"
+                    element={
+                        <Suspense
+                            fallback={<div>Идёт загрузка корзины...</div>}
+                        >
+                            <Cart />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="pizza/:id"
+                    element={
+                        <Suspense fallback={<div>Идёт загрузка...</div>}>
+                            <FullPizza />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="*"
+                    element={
+                        <Suspense fallback={<div>Идёт загрузка...</div>}>
+                            <NotFound />
+                        </Suspense>
+                    }
+                />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ export const Header: React.FC = () => {
     return (
         <div className="header">
             <div className="container">
-                <Link to="/">
+                <Link to="/pizza-delivery">
                     <div className="header__logo">
                         <img width="38" src={logoSvg} alt="Pizza logo" />
                         <div>
@@ -39,7 +39,10 @@ export const Header: React.FC = () => {
                 {location.pathname !== "/cart" && <Search />}
                 <div className="header__cart">
                     {location.pathname !== "/cart" && (
-                        <Link to="/cart" className="button button--cart">
+                        <Link
+                            to="/pizza-delivery/cart"
+                            className="button button--cart"
+                        >
                             <span>{totalPrice} ₽</span>
                             <div className="button__delimiter"></div>
                             <svg
